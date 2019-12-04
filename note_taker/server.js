e996582ea874a0b573875bb2d2d72c586df76bf8
Elizkoleary <eliz.k.oleary@gmail.com>
2019-12-04 10:46:37 -0600

    working code

--- note_taker/server.js
+++ note_taker/server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var path = require("path");
-var mysql = require("mysql");
 var fs = require("fs");
 
 var app = express();
@@ -19,19 +18,42 @@ app.listen(PORT, function () {
 });
 
 //API
-app.get("/api/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./db/db.json"));
+app.get("/api/notes", function(req, res) {
+  res.json(JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json"), 'utf8')));
 });
 
+var notes = fs.readFileSync(path.join(__dirname, "./db/db.json"), 'utf8');
+notes=JSON.parse(notes);
 
-// Create note - takes in JSON input
-app.post("/api/notes", function (req, res) {
+// Create note 
+app.post("/api/notes", function(req, res) {
   var newNote = req.body;
-
-  // activeNote.routeName = activeNote.name.replace(/\s+/g, "").toLowerCase();
-  console.log(newNote);
-
-  newNote.push(activeNote);
-
+  newNote.id = notes.length;
+  notes.push(newNote);
+  let stringifiedNotes = JSON.stringify(notes);
+  fs.writeFile("./db/db.json", stringifiedNotes, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+  });
   res.json(newNote);
 });
+
+//delete note
+app.delete("/api/notes/:id", function(req, res) {
+  const targetNote = parseInt(req.params.id);
+  notes.splice(targetNote, 1);
+  for(let i = 0; i < notes.length; i++){
+    notes[i].id = i;
+  }
+  let stringifiedNotes = JSON.stringify(notes);
+  fs.writeFile("./db/db.json", stringifiedNotes, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+  });
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
