0245293aef8d8579bf6aac398e85777e6cef2947
Elizkoleary <eliz.k.oleary@gmail.com>
2019-12-19 09:05:13 -0600

    finally working

--- hamburger_app/controllers/burgers_controllers.js
+++ hamburger_app/controllers/burgers_controllers.js
@@ -2,10 +2,10 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burgers.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 var burgers = require("../models/burgers.js");
 
-// Create all our routes and set up logic within those routes where required.
+// GET (read)
 router.get("/", function(req, res) {
   burgers.selectAll(function(data) {
     var hbsObject = {
@@ -16,46 +16,34 @@ router.get("/", function(req, res) {
   });
 });
 
+//POST (create)
 router.post("/api/burgers", function(req, res) {
-  burgers.insertOne([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
+  burgers.insertOne(req.body.burger_name, function(result) {
     res.json({ id: result.insertId });
   });
 });
 
+//PUT (update)
 router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+  var condition = req.params.id;
 
   console.log("condition", condition);
 
-  burgers.updateOne({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  burgers.updateOne(condition, function(result) {
+    res.json(result);
   });
 });
 
-// router.delete("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+//PUT (update)
+router.put("/api/burgersTwo/:id", function(req, res) {
+  var condition = req.params.id;
 
-//   burgers.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+  console.log("condition", condition);
+
+  burgers.updateTwo(condition, function(result) {
+    res.json(result);
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- hamburger_app/models/burgers.js
+++ hamburger_app/models/burgers.js
@@ -1,29 +1,28 @@
+
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burgers = {
   selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
+    orm.selectAll(function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
+  insertOne: function(burger_name, cb) {
+    orm.insertOne(burger_name, function(res) {
       cb(res);
     });
   },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
+  updateOne: function(id, cb) {
+    orm.updateOne(id, function(res) {
       cb(res);
     });
-  },
-  // delete: function(condition, cb) {
-  //   orm.delete("burgers", condition, function(res) {
-  //     cb(res);
-  //   });
-  // }
+  },  updateTwo: function(id, cb) {
+    orm.updateTwo(id, function(res) {
+      cb(res);
+    });
+  }
 };
 
-// Export the database functions for the controller (burgersController.js).
+// Export the database functions for the controller (burgerController.js).
 module.exports = burgers;
